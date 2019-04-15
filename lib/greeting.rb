1bda18bbfa3163768ae3d38887be79b2641bd280
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-15 16:07:05 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,7 @@
 # code the #greeting method here!
+
+# In order to get our tests passing, we'll need to define a method, `#greeting` that takes in an argument of a person's name and uses string interpolation to print `"Hello #{name}. It's nice to meet you."`.
+
+def greeting(name)
+  puts "Hello #{name.chomp.strip}. It's nice to meet you."
+end
